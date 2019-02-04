49c705f76bcc433ae3d99f08c6e5ca51d447cbeb
iwata <121048+iwata@users.noreply.github.com>
2019-02-04 12:21:37 +0900

    an exmaple to backup Firestore

---
+++ backup-firestore/function.go
@@ -0,0 +1,49 @@
+package gcf
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/pkg/errors"
+	"golang.org/x/oauth2/google"
+	firestore "google.golang.org/api/firestore/v1beta1"
+)
+
+var projectID string
+
+type PubSubMessage struct {
+	Data string `json:"data"`
+}
+
+func init() {
+	projectID = os.Getenv("GCP_PROJECT")
+}
+
+func BackupFirestore(ctx context.Context, m PubSubMessage) error {
+	client, err := google.DefaultClient(ctx,
+		"https://www.googleapis.com/auth/datastore",
+		"https://www.googleapis.com/auth/cloud-platform")
+	if err != nil {
+		return errors.Wrap(err, "Failed to create a Google client")
+	}
+
+	svc, err := firestore.New(client)
+	if err != nil {
+		return errors.Wrap(err, "Failed to create Firestore service")
+	}
+
+	req := &firestore.GoogleFirestoreAdminV1beta1ExportDocumentsRequest{
+		OutputUriPrefix: fmt.Sprintf("gs://%s-backup-firestore", projectID),
+	}
+	_, err = firestore.NewProjectsDatabasesService(svc).ExportDocuments(
+		fmt.Sprintf("projects/%s/databases/(default)", projectID), req,
+	).Context(ctx).Do()
+	if err != nil {
+		return errors.Wrap(err, "Failed to export Firestore")
+	}
+
+	fmt.Println("Backup Successfully")
+
+	return nil
+}
